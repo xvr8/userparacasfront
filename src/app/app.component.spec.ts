72782594d5d9511e712737b40f139ea185493a12
Carlos Rios <rt781875@gmail.com>
2024-10-28 15:39:39 -0500

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'userparacasfront' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('userparacasfront');
+    expect(app.title).toEqual('Edson El xD');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, userparacasfront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Edson el XD');
   });
 });
